ebddb1695fef6fb647b706e4ed926ce309aabba1
Renz Rackhold <rackhold@gmail.com>
2023-12-29 02:28:34 +0800

    Optimize match using sets

--- phasebook/match.py
+++ phasebook/match.py
@@ -20,8 +20,5 @@ def match(match_id):
 
 
 def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
-            return False
-
-    return True
+    # Using sets for efficient membership testing, which significantly improves performance
+    return set(fave_numbers_2).issubset(set(fave_numbers_1))
