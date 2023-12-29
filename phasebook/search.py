4fd26039ddb9a8b99c857db9b10401a8fd590c9b
Renz Rackhold <rackhold@gmail.com>
2023-12-29 18:49:18 +0800

    Implement Search Function

--- phasebook/search.py
+++ phasebook/search.py
@@ -26,5 +26,38 @@ def search_users(args):
     """
 
     # Implement search here!
+    output = []
+    #Add set to store unique ids and avoid duplicates
+    ids_seen = set()
 
-    return USERS
+    def sort_priority(user):
+        if args.get('id') == user['id']:
+            return 1
+        elif args.get('name') and args['name'].lower() in user['name'].lower():
+            return 2
+        elif args.get('age') and int(args['age']) - 1 <= user['age'] <= int(args['age']) + 1:
+            return 3
+        elif args.get('occupation') and args['occupation'].lower() in user['occupation'].lower():
+            return 4
+        else:
+            return 5 #Default priority for unmatched users
+
+
+    for key in args.keys():
+        for user in USERS:
+            user_id = user["id"]
+            if user_id in ids_seen:
+                #Skips user that already added to result
+                continue
+        
+            if(
+                (key == 'id' and args[key] == user_id) or \
+                (key in ['name', 'occupation'] and args[key].lower() in user[key].lower()) or \
+                (key == 'age' and int(args[key]) - 1 <= int(user[key]) <= int(args[key]) + 1)
+            ):
+                output.append(user)
+                ids_seen.add(user_id)
+    
+    sorted_match = sorted(output, key=sort_priority)
+
+    return sorted_match
